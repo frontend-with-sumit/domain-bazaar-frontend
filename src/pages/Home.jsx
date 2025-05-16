23da45e80ce4501b4121edcfbf108bb4472c76c5
Sumit <sk990005@outlook.com>
2025-05-16 22:50:56 +0530

    add segmented group

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import DomainSearch from '@/components/DomainSearch';
+import SegmentSlider from '@/components/shared/SegmentSlider';
 import MainLayout from '@/layouts/MainLayout';
+import { Box, SegmentGroup } from '@chakra-ui/react';
 
+const SLIDER_ITEMS = ['Domain Search', 'Trending Domains'];
 const Home = () => {
 	return (
 		<MainLayout>
 			<DomainSearch />
+			<Box width="100%" textAlign="center" marginTop={10}>
+				<SegmentSlider items={SLIDER_ITEMS} />
+			</Box>
 		</MainLayout>
 	);
 };
