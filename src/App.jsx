15a0ade248671bc747eba1e49fcf0af255059cfd
Sumit <sk990005@outlook.com>
2025-05-16 18:18:51 +0530

    setup tailwind and basic theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { Button, HStack } from '@chakra-ui/react';
+import { Button, HStack, Text } from '@chakra-ui/react';
 import './App.css';
 
 function App() {
 	return (
 		<HStack>
+			<Text>This is a dummy text</Text>
 			<Button>Click me</Button>
 		</HStack>
 	);
