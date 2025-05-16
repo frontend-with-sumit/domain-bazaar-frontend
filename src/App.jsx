40ab1df79843396132b9acb02c32f12428ff6c96
Sumit <sk990005@outlook.com>
2025-05-16 17:47:07 +0530

    setup chakra-ui

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button, HStack } from '@chakra-ui/react';
+import './App.css';
+
+function App() {
+	return (
+		<HStack>
+			<Button>Click me</Button>
+		</HStack>
+	);
+}
+
+export default App;
